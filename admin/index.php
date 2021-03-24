7f67fe12e89b4e6be6262b5325c7ba46d4405f66
AlphaRomeoMike <subjectalpha32@gmail.com>
2021-03-24 03:37:59 -0700

    :question: Updated gitignore and removed the extra file that were kept in

--- admin/index.php
+++ admin/index.php
@@ -1,17 +1,13 @@
 <?php
   include 'links/head.php';
   include 'links/nav.php';
-?>
-      
-      
-      <div class="container-fluid">
+?>    
+      <div class="container">
         <h1 class="mt-4 display-2">Welcome <?php echo $user_name; ?>!</h1>
         <p>Your User Identity is: <b><?php echo $user_id; ?></b></p>
         <p>Your User Name is: <b><?php echo $username; ?></b></p>
       </div>
     </div>
-
-  </div>
-  <?php
+<?php
   include 'links/foot.php';
 ?>
